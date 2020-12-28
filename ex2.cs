6dd6e960bb3f4b66c2e79bac2303f15ca00a30f6
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2020-12-28 09:08:30 +0700

    Add files via upload

---
+++ ex2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class ex2
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new { Name = "A", Price = 3 };
+            Console.WriteLine("Name = {0}\nPrice = {1}", p1.Name.ToLower(), p1.Price);
+            var i = 5;
+            var s = "hello";
+            Console.WriteLine("type of i: " + i.GetType());
+            //s is a string 
+            Console.WriteLine("First letter of s:" + s.Substring(1, 1));
+            Console.ReadLine();
+        }
+    }
+}
